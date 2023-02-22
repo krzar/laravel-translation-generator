96e778bf98cffaadc6a0f34a12e6a634564c2a68
Krystian Zaręba <krystian.zareba@gmail.com>
2023-02-22 22:09:38 +0100

    Generator for packages translations

--- src/Services/Generators/JsonFileGenerator.php
+++ src/Services/Generators/JsonFileGenerator.php
@@ -3,26 +3,43 @@
 namespace Krzar\LaravelTranslationGenerator\Services\Generators;
 
 use Illuminate\Support\Collection;
+use Krzar\LaravelTranslationGenerator\Exceptions\FallbackLanguageFileNotExistsException;
+use Krzar\LaravelTranslationGenerator\Services\TranslationFilesFinder;
 
 class JsonFileGenerator extends TranslationGenerator
 {
     public function generate(): void
     {
         $this->generateSingle();
+
+        if ($this->generatePackagesTranslations) {
+            $this->generatePackagesFiles();
+        }
     }
 
     protected function getTranslations(string $locale): ?Collection
     {
-        $path = lang_path("$locale.json");
+        $path = TranslationFilesFinder::jsonFile($locale, $this->currentPackage);
 
         return file_exists($path) ? collect(json_decode(file_get_contents($path), true)) : null;
     }
 
     protected function putToFile(Collection $translations): void
     {
-        $targetPath = lang_path("$this->lang.json");
+        $targetPath = TranslationFilesFinder::jsonFile($this->lang, $this->currentPackage);
         $content = json_encode($translations, JSON_PRETTY_PRINT);
 
         file_put_contents($targetPath, $content);
+    }
+
+    /**
+     * @throws FallbackLanguageFileNotExistsException
+     */
+    private function generatePackagesFiles(): void
+    {
+        $this->packagesTranslationsService->findPackages()->each(function (string $package) {
+            $this->currentPackage = $package;
+            $this->generateSingle();
+        });
     }
 }
---
+++ src/Services/TranslationFilesFinder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Krzar\LaravelTranslationGenerator\Services;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+class TranslationFilesFinder
+{
+    private const PHP_EXT = '.php';
+
+    private const JSON_EXT = '.json';
+
+    public static function phpFiles(string $lang, ?string $package = null): Collection
+    {
+        if ($package) {
+            $path = lang_path("vendor/$package/$lang");
+        } else {
+            $path = lang_path($lang);
+        }
+
+        if (file_exists($path)) {
+            return collect(scandir($path))->filter(fn (string $file) => Str::endsWith($file, self::PHP_EXT));
+        }
+
+        return collect();
+    }
+
+    public static function jsonFile(string $lang, ?string $package = null): string
+    {
+        if ($package) {
+            return lang_path("vendor/$package/$lang".self::JSON_EXT);
+        }
+
+        return lang_path($lang.self::JSON_EXT);
+    }
+}
